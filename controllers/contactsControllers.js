133e2795332d2409bdae27558f582a6bdc8771ff
nahirniy <nahirniyy@gmail.com>
2024-02-27 13:26:58 +0200

    finished task

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,64 @@
-import contactsService from "../services/contactsServices.js";
+import HttpError from "../helpers/HttpError.js";
+import * as contactsServices from "../services/contactsServices.js";
 
-export const getAllContacts = (req, res) => {};
+export const getAllContacts = async (req, res, next) => {
+  try {
+    const contacts = await contactsServices.listContacts();
+    res.json(contacts);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const getOneContact = (req, res) => {};
+export const getOneContact = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const contact = await contactsServices.getContactById(id);
 
-export const deleteContact = (req, res) => {};
+    if (!contact) throw HttpError(404);
 
-export const createContact = (req, res) => {};
+    res.json(contact);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const updateContact = (req, res) => {};
+export const deleteContact = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const contact = await contactsServices.removeContact(id);
+
+    if (!contact) throw HttpError(404);
+
+    res.status(201).json(contact);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const createContact = async (req, res, next) => {
+  const { name, email, phone } = req.body;
+
+  try {
+    const contact = await contactsServices.addContact(name, email, phone);
+
+    if (!contact) throw HttpError(404);
+
+    res.status(201).json(contact);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateContact = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const contact = await contactsServices.updateById(id, req.body);
+
+    if (!contact) throw HttpError(404);
+
+    res.status(201).json(contact);
+  } catch (error) {
+    next(error);
+  }
+};
